3bccb9617be8d0cc50ae1f686858df88201f588e
ianush <sqiud41@gmail.com>
2022-11-02 16:41:18 +0100

    Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
+using Application.Activities;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -31,20 +23,8 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt =>{
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
-                });
-            });
+            services.AddApplicationServices(_config);
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
