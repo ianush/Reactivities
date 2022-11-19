1322f39d36906020addd173ad0de61c662de41e4
ianush <sqiud41@gmail.com>
2022-11-19 14:32:27 +0100

    Section 16

--- Application/Activities/AttendeeDto.cs
+++ Application/Activities/AttendeeDto.cs
@@ -1,8 +1,11 @@
-using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Application.Profiles
+namespace Application.Activities
 {
-    public class Profile
+    public class AttendeeDto
     {
         public string Username { get; set; }
 
@@ -11,7 +14,5 @@ namespace Application.Profiles
         public string Bio { get; set; }
 
         public string Image { get; set; }
-
-        public ICollection<Photo> Photos { get; set; }
     }
 }
--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -1,19 +1,17 @@
 using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 
 
 namespace Domain
 {
-        public class AppUser : IdentityUser
+    public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
 
         public string Bio { get; set; }
 
         public ICollection<ActivityAttendee> Activities { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
     }
 }
